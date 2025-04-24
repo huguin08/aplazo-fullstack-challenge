32d312b29297891ba264617f76abd74dee86c3a4
huguin08 <104787941+huguin08@users.noreply.github.com>
2025-04-24 16:26:04 -0600

    Delete aplazo-frontend directory

--- aplazo-frontend/src/enviroments/enviroment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,  
-    // Cambia esta URL al endpoint de tu backend en producción:
-    apiUrl: 'https://api.tu-dominio.com'  
-  };
--- aplazo-frontend/src/enviroments/enviroment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: false,  
-    // Cambia esta URL al endpoint de tu backend en desarrollo:
-    apiUrl: 'http://localhost:3000'  
-  };
